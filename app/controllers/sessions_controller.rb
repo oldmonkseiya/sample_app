a8311d03f59ff568f3304d41d22c7a436d01f71a
seiyachida <wa718077@gmail.com>
2018-12-25 00:41:11 +0900

    Implemented advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
+      remember user
     else
       flash[:danger] = "Invalid email/password combination"
       render 'new'
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,16 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
 
   resources :blogs
-  root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
-  resources :users
-  
-=======
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
-  get    '/login',   to: 'sessions#new' , as: "signup"
-  post   '/login',   to: 'sessions#create' 
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users 
->>>>>>> basic-login
+  resources :users
+  
+
 end
