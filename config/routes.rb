6d1e3ff028117d10dc6909c5c2cf184c25347be6
seiyachida <wa718077@gmail.com>
2018-12-25 19:46:52 +0900

    Added account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-
-  resources :blogs
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
@@ -11,6 +9,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  
+  resources :account_activations, only: [:edit] 
 
 end
